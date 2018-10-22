b6571b159ae4a838461c4de49f57cfc4529898b3
suhas023 <notsuhas@gmail.com>
2018-10-22 23:50:23 +0530

    page now displays image

--- src/components/Compartment.js
+++ src/components/Compartment.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import BooksList from './BooksList';
 import '../styles/Compartment.css';
 
 function Compartment(props) {
-  return <h2>{props.name}</h2>;
+  let name;
+  switch(props.name) {
+    case 'currentlyReading': name = 'Currently Reading'; break;
+    case 'wantToRead': name = 'Want To Read'; break;
+    case 'read': name = 'Read'; break;
+    default: name = '--';
+  }
+  return (
+    <div className="compartment">
+      <h3>{name}</h3>
+      <BooksList books={props.books}/>
+    </div>
+  );
 }
 
 export default Compartment;
