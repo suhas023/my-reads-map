572cddbe50480cf16480292285052cc4eb42f8b2
suhas023 <notsuhas@gmail.com>
2018-10-22 21:36:14 +0530

    added bookShelf and compartment components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import BookShelf from './components/BookShelf'
+import BookShelf from './components/BookShelf';
+import * as bookAPi from './api/BooksAPI';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    allBooks : [],
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
+  }
+
+  validShelfs = ['currentlyReading', 'wantToRead', 'read'];
+
+  componentDidMount() {
+    bookAPi.getAll()
+    .then(books => {
+      let shelfs = {
+        currentlyReading: [],
+        wantToRead: [],
+        read: []
+      };
+      this.setState({allBooks: books});
+      books.forEach(book => {
+        shelfs[book.shelf].push(book);
+      });
+      for(let shelf in shelfs) {
+        this.setState({[shelf]: shelfs[shelf]})
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <BookShelf />
+        <BookShelf
+          currentlyReading={this.state.currentlyReading} 
+          wantToRead={this.state.wantToRead}
+          read={this.state.read}
+          shelfs = {this.validShelfs}
+        />
       </div>
     );
   }
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { Component } from 'react';
-import * as bookAPi from '../api/BooksAPI';
+import Compartment from './Compartment';
+import '../styles/BookShelf.css';
 
-class BookShelf extends Component {
-  state = {
-    books: [],
-  }
-
-  componentDidMount() {
-    bookAPi.getAll()
-    .then(data => this.setState({books: data}));
-  }
-
-  render() {
-    return (
-      <div className="book-shelf">
-        <header><h1>My Reads</h1></header>
-        <div className="categories">
-
-        </div>
+function BookShelf(props) {
+  let shelfs = props.shelfs;
+  return (
+    <div className="book-shelf">
+      <header><h1>My Reads</h1></header>
+      <div className="compartments">
+        {
+          shelfs.filter((shelf) => props[shelf].length > 0)
+          .map((shelf) => (
+            <Compartment
+              key={shelf}
+              name={shelf}
+              books={props[shelf]}
+            />)
+          )
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default BookShelf;
