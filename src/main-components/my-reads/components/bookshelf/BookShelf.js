fa8d6bf360ff8a911f810f87025e0459be10804d
suhas023 <notsuhas@gmail.com>
2018-12-06 02:53:16 +0530

    added map button

--- src/main-components/my-reads/components/bookshelf/BookShelf.js
+++ src/main-components/my-reads/components/bookshelf/BookShelf.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import Compartment from '../compartment/Compartment';
 import { Link } from 'react-router-dom';
+
 import './BookShelf.css';
 
 function BookShelf(props) {
   let shelfs = props.shelfs;
   return (
     <div className="book-shelf">
-      <header><h1>My Reads</h1></header>
+      <header>
+        <h1>My Reads</h1>
+      </header>
       <div className="compartments">
         {
           shelfs.filter((shelf) => props[shelf].length > 0)
-          .map((shelf) => (
-            <Compartment
-              key={shelf}
-              name={shelf}
-              books={props[shelf]}
-              changeShelf={props.changeShelf}
-            />)
-          )
+            .map((shelf) => (
+              <Compartment
+                key={shelf}
+                name={shelf}
+                books={props[shelf]}
+                changeShelf={props.changeShelf}
+              />)
+            )
         }
       </div>
       <div className="open-search">
-          <Link to='/search'>Add a book</Link>
+        <Link to="/map" className="map-btn">MAP</Link>
+        <Link to='/search' className="add-btn">Add a book</Link>
       </div>
     </div>
   );
