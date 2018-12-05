43ce8b5093fac5d3335f9ffed81875de474e5a71
suhas023 <notsuhas@gmail.com>
2018-12-06 03:59:11 +0530

    Updates

---
+++ precache-manifest.8d425d5544cef349eb852ec75fedafd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads-map/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads-map/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads-map/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "b86c4fcbfc0e34f713f6",
+    "url": "/my-reads-map/static/js/runtime~main.b86c4fcb.js"
+  },
+  {
+    "revision": "a0192e70da0336c5592d",
+    "url": "/my-reads-map/static/js/main.a0192e70.chunk.js"
+  },
+  {
+    "revision": "de03467ce7ec4b707f7a",
+    "url": "/my-reads-map/static/js/1.de03467c.chunk.js"
+  },
+  {
+    "revision": "a0192e70da0336c5592d",
+    "url": "/my-reads-map/static/css/main.2b6a49ea.chunk.css"
+  },
+  {
+    "revision": "5f2d07040048c4b74eb6625db1b81207",
+    "url": "/my-reads-map/index.html"
+  }
+];
